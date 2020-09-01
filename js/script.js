6908dca58832a1968d0cd1754281c7d431fe2cfd
drewatienza <atienza.drew@gmail.com>
2020-09-01 13:45:11 -0700

    final project

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  var typed = new Typed(".typed", {
+    strings: ["Family Owned and Operated"],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false,
+  });
+});
